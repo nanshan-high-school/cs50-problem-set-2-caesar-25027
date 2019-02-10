680357ae415527326aed2dca73de4a8409f658ad
25027劉修瑞 <44057587+25027@users.noreply.github.com>
2019-02-10 20:49:17 +0800

    Create 25027凱薩.cpp

---
+++ 25027凱薩.cpp
@@ -0,0 +1,38 @@
+# cs50-problem-set-2-caesar-25027
+cs50-problem-set-2-caesar-25027 created by GitHub Classroom
+
+#include <iostream>
+using namespace std;
+string caesar(int key, string input);
+
+int main() {
+    int key;
+    cout << "請輸入密鑰：";
+    cin >> key;
+
+    string plaintext;
+    cout << "請輸入明文：";
+    cin >> plaintext;
+
+    string ciphertext = caesar(key, plaintext);
+
+    cout << "密文為：" << ciphertext;
+}
+
+string caesar(int key, string plaintext) {
+    for (int i = 0; plaintext[i] != '\0'; i++) {
+        if (plaintext[i] >= 'A' && plaintext[i] <= 'Z') {         // 大寫
+            plaintext[i] += key;
+            if (plaintext[i] > 'Z') {
+                plaintext[i] -= 26;
+            }
+        } else if (plaintext[i] >= 'a' && plaintext[i] <= 'z') {  // 小寫
+            plaintext[i] += key;
+            if (plaintext[i] > 'z') {
+                plaintext[i] -= 26;
+            }
+        }
+    }
+
+    return plaintext;
+}
